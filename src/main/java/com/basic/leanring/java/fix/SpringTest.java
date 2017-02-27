e346b946966fc28a9827442bd889fef55e2dfa81
沉羽 <chenyu.szh@alipay.com>
2017-02-27 13:25:00 +0800

    add spring env

---
+++ src/main/java/com/basic/leanring/java/fix/SpringTest.java
@@ -0,0 +1,18 @@
+package com.basic.leanring.java.fix;
+
+/**
+ * @author sunzihan
+ * @version $Id: SpringTest.java V 0.1 2/27/17 13:21 sunzihan EXP $
+ */
+public class SpringTest {
+
+
+    public void printf(){
+
+        System.out.println("spring test");
+
+    }
+
+
+}
+
