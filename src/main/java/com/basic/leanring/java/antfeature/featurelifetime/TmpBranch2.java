b7b0f5470496775fa72881b959659d21f5be9bd1
沉羽 <chenyu.szh@alipay.com>
2017-05-22 11:07:19 +0800

    add tMP Branch

---
+++ src/main/java/com/basic/leanring/java/antfeature/featurelifetime/TmpBranch2.java
@@ -0,0 +1,9 @@
+package com.basic.leanring.java.antfeature.featurelifetime;
+
+/**
+ * @author sunzihan
+ * @version $Id: TmpBranch2.java V 0.1 5/22/17 11:04 sunzihan EXP $
+ */
+public class TmpBranch2 {
+}
+
---
+++ src/main/java/com/basic/leanring/java/antfeature/featurelifetime/TmpBranchTest.java
@@ -0,0 +1,9 @@
+package com.basic.leanring.java.antfeature.featurelifetime;
+
+/**
+ * @author sunzihan
+ * @version $Id: TmpBranchTest.java V 0.1 5/22/17 11:00 sunzihan EXP $
+ */
+public class TmpBranchTest {
+}
+
