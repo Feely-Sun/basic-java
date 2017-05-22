88018ed1449a1e12d5902241a7ce91b696cb36ef
沉羽 <chenyu.szh@alipay.com>
2017-05-22 11:07:34 +0800

    add

--- src/main/java/com/basic/leanring/java/antfeature/featurelifetime/TmpBranchTest.java
+++ src/main/java/com/basic/leanring/java/antfeature/featurelifetime/TmpBranchTest.java
@@ -5,5 +5,14 @@ package com.basic.leanring.java.antfeature.featurelifetime;
  * @version $Id: TmpBranchTest.java V 0.1 5/22/17 11:00 sunzihan EXP $
  */
 public class TmpBranchTest {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+
+    }
+
 }
 
