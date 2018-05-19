9210820de7100f061e7f7dbb501583e13f9218dc
沉羽 <chenyu.szh@alipay.com>
2018-05-19 15:13:17 +0800

    test

---
+++ src/main/java/com/basic/leanring/compoment/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author sunzihan
+ * @version $Id: package-info.java V 0.1 5/19/18 15:11 sunzihan EXP $
+ */
+package com.basic.leanring.compoment;
---
+++ src/main/java/com/basic/leanring/compoment/rule/model/T.java
@@ -0,0 +1,9 @@
+package com.basic.leanring.compoment.rule.model;
+
+/**
+ * @author sunzihan
+ * @version $Id: T.java V 0.1 5/19/18 15:12 sunzihan EXP $
+ */
+public class T {
+}
+
